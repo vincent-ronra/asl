5f110a53b920e85376abbc7a5dfd09d654c33f5c
unknown <ronravincent@gmail.com>
2020-09-26 23:47:17 +0530

    small changes in validation updated

--- js/login.js
+++ js/login.js
@@ -932,13 +932,10 @@ function validateEmail(sEmail) {
     var reEmail = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
   
     if(!sEmail.match(reEmail)) {
-      x=document.getElementById('alert-msg');
-      x.style.display="block";
-      x.style.color="red";
+      document.getElementById("error-msg").innerHTML="Invalid Email address!!";
       return false;
     }
-    x=document.getElementById('alert-msg');
-    x.style.display="none";
+    document.getElementById("error-msg").innerHTML="";
     return true;
   
   }
